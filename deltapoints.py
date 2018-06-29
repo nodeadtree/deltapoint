3e15d5a471dfa8125014020bd98407941cc02bc1
Juniper Overbeck <nodeadtree@gmail.com>
2018-06-29 13:49:29 -0700

    Added License

--- deltapoints.py
+++ deltapoints.py
@@ -1,4 +1,9 @@
 #!/bin/python
+# Copyright (c) 2018 Juniper Overbeck
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import itertools as it
 import numpy as np
 import argparse as ap
